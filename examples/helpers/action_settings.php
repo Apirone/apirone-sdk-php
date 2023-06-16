8980590dbc33b24e9f392aeb06a16c3ec1579122
alex <alex.zaytseff@gmail.com>
2023-06-16 14:06:46 +0400

    Update action_settings.php

--- examples/helpers/action_settings.php
+++ examples/helpers/action_settings.php
@@ -6,22 +6,19 @@ use Apirone\Invoice\Utils;
 use Apirone\Invoice\Model\Settings;
 
 $path = '/var/www/storage/settings.json';
-$file = (int) file_exists($path);
-
 $action = $_GET['action'] ?? false;
-
 
 switch ($action) {
     case 'create':
         require_once('../settings_create.php');
         break;
     case 'delete':
-        if ($file) {
+        if (file_exists($path)) {
             unlink($path);
         }
         break;
 }
 
-$file = (int) file_exists($path);
-$content = ($file) ? load_file_content($path,  false) : false;
+$content = file_exists($path) ? load_file_content($path,  false) : false;
+
 Utils::send_json($content);
