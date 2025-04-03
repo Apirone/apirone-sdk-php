234371ad6dcfb6321c3bdf0a89d9e3c0a8a2e495
Alex Zaytseff <alex.zaytseff@gmail.com>
2025-04-03 18:04:52 +0400

    Examples loggerCallback updated

--- examples/log.php
+++ examples/log.php
@@ -14,12 +14,11 @@ require_once('helpers/common.php');
 use Apirone\SDK\Invoice;
 
 $loggerCallback = static function ($level, $message, $context) {
-    // if ($level = \Apirone\API\Log\LogLevel::INFO) {
-    //     return;
-    // }
     $log_file = '/var/www/storage/log.txt';
-    $data = [$level, $message, $context];
-    file_put_contents($log_file, print_r($data, true) . "\r\n", FILE_APPEND);
+    $dt = new \DateTime();
+    $context = ($context) ? ' CONTEXT: ' . json_encode($context) : '';
+    $logdata = sprintf('%s %s %s%s', $dt->format("Y-m-d\TH:i:sP"), strtoupper($level), $message, $context);
+    file_put_contents($log_file, $logdata. "\r\n", FILE_APPEND);
 };
 
 Invoice::logger($loggerCallback);
